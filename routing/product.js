ea40b86afd73749ce2edce6d6b2b671fe53129d7
incmrijdty <alesia.sichova@yandex.by>
2025-04-08 18:34:55 +0200

    Update product.js

--- routing/product.js
+++ routing/product.js
@@ -1,37 +1,50 @@
-const fileSystem = require("fs");
-const { STATUS_CODE } = require("../constants/statusCode");
-
-const path = require("path");
-
 const express = require("express");
-
-const renderNewProductPage = require("../views/renderNewProductPage");
-
+const { MENU_LINKS } = require("../constants/navigation");
+const { productsSlice } = require("../store/products"); 
 const router = express.Router();
 
-router.get("/add", (_request, response) => {
-  response.sendFile(path.join(__dirname, "../views", "add-product.html"));
+
+router.get("/", (_request, response) => {
+  response.render("products", {
+    headTitle: "Shop – Products",
+    path: "/products",
+    menuLinks: MENU_LINKS,
+    activeLinkPath: "/products",
+    products: productsSlice.products 
+  });
 });
+
+
+router.get("/add", (_request, response) => {
+  response.render("add-product", {
+    headTitle: "Shop - Add product",
+    path: "/products/add",
+    menuLinks: MENU_LINKS,
+    activeLinkPath: "/products/add"
+  });
+});
+
 
 router.post("/add", (request, response) => {
   const { name, description } = request.body;
 
-  fileSystem.writeFile(
-    "product.txt",
-    `Name: ${name}, Description: ${description}`,
-    (error) => {
-      if (error) {
-        throw error;
-      }
+  
+  const newProduct = { name, description };
+  productsSlice.products.push(newProduct);
+  productsSlice.newestProduct = newProduct;
 
-      response.status(STATUS_CODE.FOUND).redirect("/product/new");
-    }
-  );
+  response.redirect("/products/new");
 });
 
+
 router.get("/new", (_request, response) => {
-  fileSystem.readFile("product.txt", "utf-8", (_error, data) => {
-    response.send(renderNewProductPage(data));
+  const newestProduct = productsSlice.newestProduct;
+  response.render("new-product", {
+    headTitle: "Shop – Newest product",
+    path: "/products/new",
+    menuLinks: MENU_LINKS,
+    activeLinkPath: "/products/new",
+    newestProduct: newestProduct ? newestProduct : "No data available" 
   });
 });
 
